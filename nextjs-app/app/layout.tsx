57df11f7edfe981582d72127252c4f89b58b5ba6
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2025-03-19 21:47:23 +0100

    fix(placeholder): update placeholder text

--- nextjs-app/app/layout.tsx
+++ nextjs-app/app/layout.tsx
@@ -8,8 +8,6 @@ import { VisualEditing, toPlainText } from 'next-sanity';
 import { Toaster } from 'sonner';
 
 import DraftModeToast from '@/app/components/DraftModeToast';
-import Footer from '@/app/components/Footer';
-import Header from '@/app/components/Header';
 import * as demo from '@/sanity/lib/demo';
 import { sanityFetch, SanityLive } from '@/sanity/lib/live';
 import { settingsQuery } from '@/sanity/lib/queries';
