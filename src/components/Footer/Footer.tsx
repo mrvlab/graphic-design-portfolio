70563b9a77dc34a442597d8f9da644465c710f0f
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2025-03-31 23:55:27 +0200

    feat(footer): add CreativeServices icon for responsive design in the footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,6 +5,8 @@ import { fetchFooterQuery } from '@/sanity/lib/queries';
 import { FetchFooterQueryResult } from '../../../sanity.types';
 import { sanityFetch } from '@/sanity/lib/live';
 import { PortableText } from 'next-sanity';
+import CreativeServicesMobile from './CreativeServicesMobile';
+import CreativeServicesDesktop from './CreativeServicesDesktop';
 
 const Footer = async () => {
   const { data: footer }: { data: FetchFooterQueryResult } = await sanityFetch({
@@ -12,7 +14,7 @@ const Footer = async () => {
   });
 
   return (
-    <footer className='flex flex-col items-center py-4 lg:flex-row lg:px-2 lg:pt-[25px] lg:pb-3 lg:justify-between backdrop-blur-md'>
+    <footer className='flex flex-col items-center pt-[21px] pb-[18px] lg:flex-row lg:px-2 lg:pt-[25px] lg:pb-3 lg:justify-between backdrop-blur-md relative'>
       <div className='hidden lg:flex lg:pt-3 lg:pb-1'>
         {footer?.lefttext && <PortableText value={footer?.lefttext} />}
       </div>
@@ -23,6 +25,15 @@ const Footer = async () => {
         &copy; {getCurrentYear()}
         <span className='hidden lg:flex lg:justify-center'>{footer?.name}</span>
         {footer?.rights}
+      </div>
+      <div className='absolute inset-0 flex items-center justify-center'>
+        <div className='absolute top-4 lg:hidden'>
+          <CreativeServicesMobile />
+        </div>
+
+        <div className='hidden lg:flex'>
+          <CreativeServicesDesktop />
+        </div>
       </div>
     </footer>
   );
