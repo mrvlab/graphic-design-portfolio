65825fd02dd5191dca5b0fe3a0bb227b29414644
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2025-04-21 14:56:16 +0200

    feat(AnimatedText): working particular text

--- src/components/AnimatedText/AnimatedText.tsx
+++ src/components/AnimatedText/AnimatedText.tsx
@@ -11,6 +11,7 @@ const GRID_GAP = 14;
 const X_PATH =
   'M1.12745 0.671875L6.89741 6.70064M0.998047 6.57124L7.02681 0.801275';
 
+// SVG paths for the text outline
 const svgString = `<svg width="2485" height="208" viewBox="0 0 2485 208" fill="none" xmlns="http://www.w3.org/2000/svg">
 <g clip-path="url(#clip0_2172_13730)">
 <path d="M641.8 152.4C641.5 166.3 642.1 180.3 641.8 194.2H655.8V208.1H697.7V194.2H725.6V180.3H739.6V166.4H753.6V152.5H767.6V194.3H781.6V208.2H809.5V194.3H837.4V180.4H851.4V166.5H865.4V152.6H879.4V194.4H893.4V208.3H935.3V194.4H963.2V180.5H977.2V166.6H1033V180.5H1047V194.4H1061V208.3H1102.9V194.4H1130.8V180.5H1158.7V166.6H1172.7V152.7H1186.7V138.8H1200.7V124.9H1214.7V97.1002H1186.8V111H1172.8V124.9C1168.1 124.9 1163.4 124.8 1158.8 124.9C1158.7 129.5 1158.8 134.2 1158.8 138.8C1149.5 138.8 1140.2 138.8 1130.9 138.8C1130.9 143.4 1130.9 148.1 1130.9 152.7H1116.9V166.6H1089V180.6H1076L1075 166.6H1061V138.8C1084.2 138.7 1107.6 139 1130.8 138.8C1130.8 134.2 1130.8 129.5 1130.8 124.9C1140.1 124.7 1149.4 125.1 1158.7 124.9C1159 111 1158.4 97.0002 1158.7 83.1002H1144.7V69.2002H1088.9V83.1002H1074.9V97.0002H1047V110.9H1033V124.8H1019V138.7H1005V83.0002H991V69.1002H963.1V96.9002H977.1V138.7H963.2V152.6H949.2V166.5H921.3V180.4H907.3V152.6H921.3V138.7H935.3V110.9H949.3V69.1002H921.4V83.0002H893.5V96.9002H879.5V110.8H865.5V124.7H851.5V138.6H837.5V152.5H823.5V166.4H795.6V152.5H809.6V138.6H823.6V124.7H837.6V69.0002H809.7V82.9002H795.7V96.8002C786.4 96.8002 777.1 96.8002 767.8 96.8002C767.7 101.4 767.8 106.1 767.8 110.7H753.8V124.6H739.8V138.5H725.8V152.4H711.8V166.3H697.8V180.2H669.9V152.4H683.8V138.5H697.8V124.6H711.8V110.7H725.8V96.8002C739.8 96.8002 753.8 96.9002 767.7 96.8002C767.9 87.5002 767.5 78.2002 767.7 68.9002L753.7 67.9002V55.0002H767.7V13.2002H739.8V41.3002H725.9V55.2002H711.9V69.1002H670V83.0002H656V96.9002H642V110.8H628V124.7H614V138.6H600V152.5C595.3 152.5 590.6 152.5 586 152.5L586.3 152.7M641.8 152.4C646.4 152.4 651.1 152.4 655.8 152.4V138.5H641.8C641.8 143.1 641.9 147.8 641.8 152.4ZM641.8 152.4C637.2 152.4 632.5 152.4 627.8 152.4V166.3H613.8V180.2H599.8V194.1H571.9V208H544V194.1H516.1V208H474.2V194.1H460.2V180.2H432.3V194.1H404.4V208H362.5V194.1H348.5V180.2H334.5V166.3H320.5V180.2H306.5V194.1H278.6V208H236.7V194.1H222.7V138.4H236.7V124.5H208.8V96.7002H222.8V82.8002H236.8V68.9002H250.8V96.8002H264.8V82.9002H278.8V69.0002H320.7V82.9002H334.7C334.5 92.2002 334.9 101.5 334.7 110.8C330.1 110.8 325.4 110.8 320.7 110.8C320.7 115.4 320.7 120.1 320.7 124.7C306.8 125 292.7 124.5 278.8 124.7V138.6H264.8V152.5H250.9V180.3H264.9V166.4H292.8V152.5H306.8V138.6H320.8C320.8 134 320.8 129.3 320.8 124.7C325.4 124.6 330.1 124.7 334.8 124.7C334.8 120.1 334.7 115.4 334.8 110.8C339.4 110.8 344.1 110.8 348.8 110.8V96.9002H376.7V83.0002H390.7V69.1002H446.5V83.0002H460.5C460.2 96.9002 460.8 110.9 460.5 124.8C451.2 125 441.9 124.6 432.6 124.8C432.6 129.4 432.6 134.1 432.6 138.7C409.4 138.9 386 138.6 362.8 138.7V166.5H376.8L377.8 180.5H390.8V166.5H418.7V152.6H432.7C432.7 148 432.7 143.3 432.7 138.7C442 138.7 451.3 138.7 460.6 138.7C460.6 134.1 460.5 129.4 460.6 124.8C465.2 124.7 469.9 124.8 474.6 124.8V110.9H488.6V97.0002H502.6V83.1002H530.5V69.2002H572.4V83.1002H600.3V69.2002H614.3V111H600.3V124.9H586.3C586.2 134.1 586.5 143.5 586.3 152.7M586.3 152.7C581.7 152.7 577 152.7 572.3 152.7V166.6H586.3C586.3 162 586.2 157.3 586.3 152.7ZM418.5 96.8002H404.5V110.7H418.5V96.8002ZM558.1 96.8002H530.2V110.7H516.2V124.6H502.2V138.5H488.3V180.3H502.3V166.4H530.2V152.5H544.2V124.7H558.2V96.9002L558.1 96.8002ZM1116.4 96.8002H1102.4V110.7H1116.4V96.8002Z" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
@@ -20,12 +21,18 @@ const svgString = `<svg width="2485" height="208" viewBox="0 0 2485 208" fill="n
 <path d="M2079.6 13.2002V55.0002H2037.7V41.1002H2051.6V13.2002H2079.6Z" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M893.1 13.1997V41.0997H879.1V54.9997H851.2V27.1997H865.2V13.1997H893.1Z" stroke="black" stroke-linecap="round" stroke-linejoin="round"/>
 </g>
+<defs>
+<clipPath id="clip0_2172_13730">
+<rect width="2484.1" height="207.4" fill="white"/>
+</clipPath>
+</defs>
 </svg>
 `;
 
 // Convert SVG to path
 const TEXT_PATHS = convertSvgToPath(svgString);
 
+// Particle class to manage particle state
 class Particle {
   targetX: number;
   targetY: number;
@@ -40,6 +47,52 @@ class Particle {
   }
 }
 
+// Utility function to create particles from SVG paths
+function createParticlesFromPaths(
+  pathElements: SVGPathElement[],
+  svgWidth: number,
+  svgHeight: number
+): Particle[] {
+  const particles: Particle[] = [];
+
+  // Create grid of points
+  for (let x = 0; x < svgWidth; x += GRID_GAP) {
+    for (let y = 0; y < svgHeight; y += GRID_GAP) {
+      const point = document
+        .createElementNS('http://www.w3.org/2000/svg', 'svg')
+        .createSVGPoint();
+      point.x = x;
+      point.y = y;
+
+      // Check if point is inside any path
+      const isInside = pathElements.some((path) => {
+        try {
+          return path.isPointInFill(point);
+        } catch (e) {
+          console.error('Error checking point:', e);
+          return false;
+        }
+      });
+
+      if (isInside) {
+        const randAngle = Math.random() * Math.PI * 2;
+        const randDist = Math.random() * 300;
+
+        particles.push(
+          new Particle(
+            x,
+            y,
+            x + Math.cos(randAngle) * randDist,
+            y + Math.sin(randAngle) * randDist
+          )
+        );
+      }
+    }
+  }
+
+  return particles;
+}
+
 export default function AnimatedText() {
   const [particles, setParticles] = useState<Particle[]>([]);
   const [scale, setScale] = useState(1);
@@ -47,14 +100,11 @@ export default function AnimatedText() {
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    console.log('AnimatedText useEffect running');
-
-    if (!containerRef.current) {
-      console.log('Refs not ready yet');
-      return;
-    }
+    if (!containerRef.current) return;
 
     const container = containerRef.current;
+    const svgWidth = 2485;
+    const svgHeight = 208;
 
     // Create a temporary SVG element
     const tempSvg = document.createElementNS(
@@ -65,10 +115,6 @@ export default function AnimatedText() {
     tempSvg.setAttribute('height', '208');
     tempSvg.setAttribute('viewBox', '0 0 2485 208');
     document.body.appendChild(tempSvg);
-
-    const newParticles: Particle[] = [];
-    const svgWidth = 2485;
-    const svgHeight = 208;
 
     // Create paths and add to temp SVG
     const pathElements = TEXT_PATHS.map((pathData) => {
@@ -82,40 +128,12 @@ export default function AnimatedText() {
       return path;
     });
 
-    // Create grid of points
-    for (let x = 0; x < svgWidth; x += GRID_GAP) {
-      for (let y = 0; y < svgHeight; y += GRID_GAP) {
-        const point = tempSvg.createSVGPoint();
-        point.x = x;
-        point.y = y;
-
-        // Check if point is inside any path
-        const isInside = pathElements.some((path) => {
-          try {
-            return path.isPointInFill(point);
-          } catch (e) {
-            console.error('Error checking point:', e);
-            return false;
-          }
-        });
-
-        if (isInside) {
-          const randAngle = Math.random() * Math.PI * 2;
-          const randDist = Math.random() * 300;
-
-          newParticles.push(
-            new Particle(
-              x,
-              y,
-              x + Math.cos(randAngle) * randDist,
-              y + Math.sin(randAngle) * randDist
-            )
-          );
-        }
-      }
-    }
-
-    console.log(`Created ${newParticles.length} particles`);
+    // Create particles from paths
+    const newParticles = createParticlesFromPaths(
+      pathElements,
+      svgWidth,
+      svgHeight
+    );
 
     // Cleanup
     document.body.removeChild(tempSvg);
@@ -155,14 +173,12 @@ export default function AnimatedText() {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  console.log('Rendering with', particles.length, 'particles');
-
   return (
     <div
       ref={containerRef}
       className='relative w-full h-full flex items-center justify-center bg-white'
     >
-      <div
+      <motion.div
         className='relative'
         style={{
           transform: `scale(${scale})`,
@@ -186,7 +202,7 @@ export default function AnimatedText() {
             }}
             transition={{
               type: 'spring',
-              damping: 20,
+              damping: 80,
               stiffness: 100,
               mass: 0.5,
               duration: 2,
@@ -206,7 +222,7 @@ export default function AnimatedText() {
             </svg>
           </motion.div>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 }
