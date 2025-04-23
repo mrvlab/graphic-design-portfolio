3f371ae66d99fcaa984316041f854fc89c07a0c8
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2025-04-23 22:22:41 +0200

    fix(NavigationMenu): increase z-index for improved visibility

--- src/components/NavBar/NavigationMenu.tsx
+++ src/components/NavBar/NavigationMenu.tsx
@@ -60,7 +60,7 @@ const NavigationMenu = async ({
       {!hideSecondSection && (
         <Link
           href='/'
-          className={`${visibilityClasses} flex flex-col items-center relative w-full z-10 pt-4 lg:pt-0 lg:col-span-16`}
+          className={`${visibilityClasses} flex flex-col items-center relative w-full z-50 pt-4 lg:pt-0 lg:col-span-16`}
           aria-label={`${header?.name} - ${header?.workTitle}`}
         >
           <h1>{header?.name}</h1>
