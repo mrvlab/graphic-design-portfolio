5704339da6c934bffbbe02b0adfafff1678c13c7
Marvin Kiyingi <72855673+MarvinxKiyingi@users.noreply.github.com>
2025-04-23 20:32:27 +0200

    refactor(layout): remove CustomCursor component integration from layout and clean up related imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import './globals.css';
 import { lausanne } from '@/fonts/Lausanne';
 import { Metadata } from 'next';
-import CustomCursor from '@/components/CustomCursor/CustomCursor';
 
 export const metadata: Metadata = {
   title: 'Martina Quirici',
@@ -15,10 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={lausanne.className}>
-        <CustomCursor />
-        {children}
-      </body>
+      <body className={lausanne.className}>{children}</body>
     </html>
   );
 }
--- src/app/project/[slug]/page.tsx
+++ src/app/project/[slug]/page.tsx
@@ -8,7 +8,6 @@ import RelatedProducts from '../RelatedProducts/RelatedProducts/RelatedProducts'
 import FirstLayout from '../ProjectLayouts/FirstLayout/FirstLayout';
 import SecondLayout from '../ProjectLayouts/SecondLayout/SecondLayout';
 import ThirdLayout from '../ProjectLayouts/ThirdLayout/ThirdLayout';
-import CustomCursor from '../CustomCursor/CustomCursor';
 
 export async function generateStaticParams() {
   const projects = await client.fetch(projectsQuery);
@@ -55,11 +54,7 @@ export default async function Page({
   const layout = project.layout;
 
   return (
-    <div
-      className='flex flex-col h-full relative cursor-none'
-      id='project-page'
-    >
-      <CustomCursor />
+    <div className='flex flex-col h-full relative' id='project-page'>
       {(() => {
         switch (layout) {
           case 'firstLayout':
